e2568ae5b916174bbadd9ce4a2ed2f83cdc2e95c
Vxle187 <ercin.raphael@gmail.com>
2025-08-31 15:39:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import os
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+# Intents aktivieren
+intents = discord.Intents.default()
+intents.message_content = True  # Wichtig, sonst liest der Bot keine Nachrichten
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
